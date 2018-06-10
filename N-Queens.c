b0688b6cfb87a9be4e3b012931a36618bf23e760
cruzer3008 <31630057+cruzer3008@users.noreply.github.com>
2018-06-10 17:42:33 +0530

    Update N-Queens.c

--- N-Queens.c
+++ N-Queens.c
@@ -1,75 +1,87 @@
+
 #include<stdio.h>
-#include<stdlib.h>
 
-int place(int x[], int k)
-{
-	int i;
-	for(i=1;i<k;i++)
-	if(x[i]==x[k]||(abs(x[i]-x[k]))==abs(i-k))
- 		return 0;
-	return 1;
-}
+#include<math.h>
+int place(int pos);
+void printsol(int k);
+void queens(int n);
 
-
-void printsolution(int n,int x[])
-{
-	int i,j;
-	char c[10][10];
-	for(i=1;i<=n;i++)
-	{
- 		for(j=1;j<=n;j++)
- 		c[i][j]='X';
-	}
-
-	for(i=1;i<=n;i++)
- 		c[i][x[i]]='Q';
-
-	for(i=1;i<=n;i++)
-	{
- 		for(j=1;j<=n;j++)
- 		{
-  			printf("%c\t",c[i][j]);
- 		}
- 		printf("\n");
-	}
-}
-
-void nqueens(int n)
-{
-	int x[10],count=0,k=1;
-	x[k]=0;
-	while(k!=0)
-	{
-		x[k]=x[k]+1;
-		while(x[k]<=n&&(!place(x,k)))
-			x[k]=x[k]+1;
-		if(x[k]<=n)
-		{
-			if(k==n)
- 			{
-  				count++;
-  				printf("\nsolution %d\n",count);
-  				printsolution(n,x);
-  			}
-  			else
-  			{
-  				k++;
-  				x[k]=0;
-  			}
- 		}
- 		
-		else
- 		{
-  			k--;
- 		}
-	}
-
-}
+int count=0,n;
+int a[100];
 
 void main()
 {
-	int n;
-	printf("Enter the no of queens\n");
+	printf("Enter the number of queens\n");
 	scanf("%d",&n);
-	nqueens(n);
+	queens(n);
+}
+
+
+void queens(int n)
+{
+	int k=1;
+	a[k]=0;
+
+	while(k!=0)
+	{
+		do
+		{
+			a[k]++;
+		}while(a[k]<=n && !(place(k)));
+	
+		if(a[k]<=n)
+		{
+			if(k==n)
+				printsol(k);
+			else
+			{
+				k++;
+				a[k]=0;
+			}
+		}
+		
+		else
+		{
+			k--;
+		}
+	}
+}
+
+int place(int pos)
+{
+	int i;
+
+	for(i=1;i<pos;i++)
+	{
+		if((a[i]==a[pos]) || (abs(a[i]-a[pos])==abs(i-pos)))
+		{
+			return 0;
+		}
+	}
+	
+	return 1;
+}
+
+void printsol(int k)
+{
+	int i,j;
+	
+	count++;
+	printf("Solution=%d\n\n",count);
+	
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			if(a[i]==j)
+			{
+				printf("Q\t");
+			}
+		
+			else	
+				printf("*\t");
+		}	
+	
+		printf("\n");
+	}
 }
